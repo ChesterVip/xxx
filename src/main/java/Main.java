f696a1b756a711e88a695c785836c8617e08b195
Marius Sokolovski <marius.sokolovsky@gmail.com>
2023-01-25 13:13:16 +0100

    changes done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
             Main.displayText();
     }
 
     private static void displayText(){
         System.out.println("Hello kurwa git");
+=======
+        System.out.println("Hello chuju z gita git");
+        System.out.println("Wprowadź chuju imię:");
+>>>>>>> 7233c05 (sout(zmieniony))
     }
 }
