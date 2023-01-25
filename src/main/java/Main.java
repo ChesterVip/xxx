5ec8e3350d717c0317c108ec1580c93f9adb82c9
Marius Sokolovski <marius.sokolovsky@gmail.com>
2023-01-25 13:20:44 +0100

    alldone

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
             Main.displayText();
     }
     private static void displayText(){
-        System.out.println("Hello kurwa git")
+        System.out.println("Hello kurwa git");
         System.out.println("Hello chuju z gita git");
         System.out.println("Wprowadź chuju imię:");
     }
