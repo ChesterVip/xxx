b1278f603a408ffcde3c9b1d57fe2205fed13a2d
Marius Sokolovski <marius.sokolovsky@gmail.com>
2023-01-25 13:32:27 +0100

    all done Maciej rebase zussammen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Main.displayText();
+            Main.displayText();
     }
     private static void displayText(){
+        System.out.println("Hello kurwa Maciej");
         System.out.println("Hello kurwa Mariusz");
         System.out.println("Hello chuju z gita git");
         System.out.println("Wprowadź chuju imię:");
