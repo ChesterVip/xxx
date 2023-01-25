2e0dd40ff442b9a489364d6996cadc6f6fd7abc6
Marius Sokolovski <marius.sokolovsky@gmail.com>
2023-01-25 13:03:19 +0100

    zmiana w drugim repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+            Main.displayText();
+    }
+
+    private static void displayText(){
         System.out.println("Hello kurwa git");
     }
 }
